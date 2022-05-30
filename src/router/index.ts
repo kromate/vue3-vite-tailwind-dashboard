2392d56473998ed5ad594c776d757996b9be2734
kromate <akpananthony33@gmail.com>
2022-05-30 04:25:39 +0100

    auto-routes

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// @ts-ignore
+import routes from '~pages'
+
+console.log(routes);
+
+const router = createRouter({ history: createWebHistory(), routes })
+
+// router.afterEach(() => {
+// 	window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+// })
+
+export { router }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Pages({
+      dirs: 'src/views',
+      routeStyle: 'nuxt',
+      extendRoute: (route: any) => {
+        const path = route.path.split('/')
+        const lastIndex = path.length - 1
+        if (path[lastIndex] && path[lastIndex].includes(':')) path[lastIndex] = path[lastIndex] + '/'
+        return { ...route, path: path.join('/') }
+      }
+    }),
+  ]
 })
