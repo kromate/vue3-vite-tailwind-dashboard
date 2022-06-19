b9cb45d3be058d367b65bf279742a32c80bc8b45
kromate <akpananthony33@gmail.com>
2022-06-19 04:23:10 +0100

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,30 +1,31 @@
 <template>
   <DefaultLayout>
     Hello World!
-    <VueApexCharts width="220" height="90" type="bar" :options="chartOptions" :series="series"></VueApexCharts>
+    <div id="world-map" style="width: 600px; height: 400px"></div>
+    <!-- <VueApexCharts width="220" height="90" type="bar" :options="chartOptions" :series="series"></VueApexCharts> -->
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
-import VueApexCharts from "vue3-apexcharts";
+// import VueApexCharts from "vue3-apexcharts";
 
-const chartOptions = {
-	chart: {
-		id: "basic-bar",
-		zoom: {
-			enabled: false,
-			type: "y",
-			autoScaleYaxis: true,
-		},
-	},
-};
-const series = [
-	{
-		name: "series-1",
-		data: [30, 40, 45, 50, 49, 60, 70, 91],
-	},
-];
+// const chartOptions = {
+// 	chart: {
+// 		id: "basic-bar",
+// 		zoom: {
+// 			enabled: false,
+// 			type: "y",
+// 			autoScaleYaxis: true,
+// 		},
+// 	},
+// };
+// const series = [
+// 	{
+// 		name: "series-1",
+// 		data: [30, 40, 45, 50, 49, 60, 70, 91],
+// 	},
+// ];
 </script>
 
 <style scoped></style>
