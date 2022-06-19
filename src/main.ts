3e2b3cc302734236a02bae1da1f183c98c59b163
kromate <akpananthony33@gmail.com>
2022-06-19 04:06:33 +0100

    feat: added chartjs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import './assets/style/tailwind.css'
 import App from './App.vue'
-import VueApexCharts from "vue3-apexcharts";
+
 import { router } from './router/index'
 
 
 const app = createApp(App);
-app.use(VueApexCharts);
 app.use(router)
 app.mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <DefaultLayout> Hello World! </DefaultLayout>
+  <DefaultLayout>
+    Hello World!
+    <VueApexCharts width="220" height="90" type="bar" :options="chartOptions" :series="series"></VueApexCharts>
+  </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import VueApexCharts from "vue3-apexcharts";
+
+const chartOptions = {
+	chart: {
+		id: "basic-bar",
+		zoom: {
+			enabled: false,
+			type: "y",
+			autoScaleYaxis: true,
+		},
+	},
+};
+const series = [
+	{
+		name: "series-1",
+		data: [30, 40, 45, 50, 49, 60, 70, 91],
+	},
+];
 </script>
 
 <style scoped></style>
