cc011f913828e07c9f10be7e576a537efdfec432
kromate <akpananthony33@gmail.com>
2022-06-19 23:33:31 +0100

    feat: update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,38 +1,17 @@
 <template>
   <DefaultLayout>
-    Hello World!
-    <div id="world-map" style="width: 600px; height: 400px"></div>
-    <!-- <VueApexCharts width="220" height="90" type="bar" :options="chartOptions" :series="series"></VueApexCharts> -->
+    <div class="row">
+      <div class="card h-52 w-1/4"></div>
+      <div class="card h-52 w-1/4"></div>
+      <div class="card h-52 w-1/4"></div>
+      <div class="card h-52 w-1/4"></div>
+    </div>
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import { onMounted } from "vue";
-
-onMounted(() => {
-  $(function () {
-    $("#world-map").vectorMap({ map: "world_mill" });
-  });
-});
-// import VueApexCharts from "vue3-apexcharts";
-
-// const chartOptions = {
-// 	chart: {
-// 		id: "basic-bar",
-// 		zoom: {
-// 			enabled: false,
-// 			type: "y",
-// 			autoScaleYaxis: true,
-// 		},
-// 	},
-// };
-// const series = [
-// 	{
-// 		name: "series-1",
-// 		data: [30, 40, 45, 50, 49, 60, 70, 91],
-// 	},
-// ];
 </script>
 
 <style scoped></style>
