23e2562a9dadd046367aa1843e2f9cdf2263ae3a
kromate <akpananthony33@gmail.com>
2022-06-22 18:31:25 +0100

    feat: update

--- src/components/pages/Dashboard/ThirdRow.vue
+++ src/components/pages/Dashboard/ThirdRow.vue
@@ -5,8 +5,34 @@
         <span class="card_title_1">TOP PRODUCTS</span>
         <i class="las la-expand" />
       </div>
+
+      <div class="flex flex-col w-full">
+        <div class="flex w-full items-center justify-between py-2.5 px-2 text-xs h-[60px]">
+          <div class="bg-[#ffffffbf] w-2/12 relative max-w-[80px] h-[60px] mr-3">
+            <span class="bg-primary h-5 w-5 center absolute top-0 left-0 text-black text-[10px]">1</span>
+          </div>
+
+          <div class="w-5/12 flex flex-col items-start justify-between gap-1">
+            <span
+              class="text-[9.18px] text-center font-medium min-w-[70px] bg-[#ffffff40] py-0.5 leading-3 px-1.5"
+            >SKU90400</span>
+            <span class="font-medium text-white">Huawei Smart Watch</span>
+            <span class="font-light">$399.00</span>
+          </div>
+
+          <div class="w-5/12 flex flex-col items-start justify-between gap-1">
+            <span
+              class="text-[9.18px] text-center font-medium min-w-[70px] bg-[#ffffff40] py-0.5 leading-3 px-1.5"
+            >SKU90400</span>
+            <span class="font-medium text-white">Huawei Smart Watch</span>
+            <span class="font-light">$399.00</span>
+          </div>
+          <i class="las la-search -rotate-90 text-sm" />
+        </div>
+      </div>
     </Card>
   </div>
+
   <div class="h-full cols-2">
     <Card>
       <div class="flex w-full items-center justify-between mb-3">
@@ -26,19 +52,17 @@
                 'rounded-full h-1.5 w-1.5 mr-3',
               ]"
             />
-            {{ item.details }}</span
-          >
+            {{ item.details }}
+          </span>
           <span class="w-3/12">{{ item.date }}</span>
           <div
             :class="[
               item.bg ? 'bg-primary text-black' : 'bg-[#ffffff40] text-white',
-              ,
+
               ' text-[9.18px] font-medium min-w-[70px] center',
             ]"
-          >
-            {{ item.type }}
-          </div>
-          <i class="las la-search -rotate-90 mr-1" />
+          >{{ item.type }}</div>
+          <i class="las la-search -rotate-90" />
         </div>
       </div>
     </Card>
@@ -47,71 +71,71 @@
 
 <script setup lang="ts">
 const activity = [
-  {
-    details: "You have sold an item - $1,299",
-    date: "just now",
-    type: "PRODUCT",
-    bg: true,
-  },
-  {
-    details: "Firewall upgrade",
-    date: "	1 min ago",
-    type: "SERVER",
-    bg: false,
-  },
-  {
-    details: "Push notification v2.0 installation",
-    date: "1 min ago",
-    type: "ANDROID",
-    bg: false,
-  },
-  {
-    details: "New Subscription - 1yr Plan",
-    date: "1 min ago",
-    type: "SALES",
-    bg: true,
-  },
-  {
-    details: "2 Unread enquiry",
-    date: "2 mins ago",
-    type: "ENQUIRY",
-    bg: false,
-  },
-  {
-    details: "$30,402 received from Paypal",
-    date: "2 mins ago",
-    type: "PAYMENT",
-    bg: true,
-  },
-  {
-    details: "3 payment received",
-    date: "5 mins ago",
-    type: "PAYMENT",
-    bg: true,
-  },
-  {
-    details: "1 pull request from github",
-    date: "5 mins ago",
-    type: "GITHUB",
-    bg: false,
-  },
-  {
-    details: "3 pending invoice to generate",
-    date: "5 mins ago",
-    type: "INVOICE",
-    bg: false,
-  },
-  {
-    details: "2 new message from fb messenger",
-    date: "7 mins ago",
-    type: "INBOX",
-    bg: false,
-  },
+	{
+		details: "You have sold an item - $1,299",
+		date: "just now",
+		type: "PRODUCT",
+		bg: true,
+	},
+	{
+		details: "Firewall upgrade",
+		date: "	1 min ago",
+		type: "SERVER",
+		bg: false,
+	},
+	{
+		details: "Push notification v2.0 installation",
+		date: "1 min ago",
+		type: "ANDROID",
+		bg: false,
+	},
+	{
+		details: "New Subscription - 1yr Plan",
+		date: "1 min ago",
+		type: "SALES",
+		bg: true,
+	},
+	{
+		details: "2 Unread enquiry",
+		date: "2 mins ago",
+		type: "ENQUIRY",
+		bg: false,
+	},
+	{
+		details: "$30,402 received from Paypal",
+		date: "2 mins ago",
+		type: "PAYMENT",
+		bg: true,
+	},
+	{
+		details: "3 payment received",
+		date: "5 mins ago",
+		type: "PAYMENT",
+		bg: true,
+	},
+	{
+		details: "1 pull request from github",
+		date: "5 mins ago",
+		type: "GITHUB",
+		bg: false,
+	},
+	{
+		details: "3 pending invoice to generate",
+		date: "5 mins ago",
+		type: "INVOICE",
+		bg: false,
+	},
+	{
+		details: "2 new message from fb messenger",
+		date: "7 mins ago",
+		type: "INBOX",
+		bg: false,
+	},
 ];
 </script>
 
 <style scoped>
 .prod_table:nth-child(2n + 1) {
-  background: rgb(255, 255, 255, 0.1);
+	background: rgb(255, 255, 255, 0.1);
 }
 </style>
